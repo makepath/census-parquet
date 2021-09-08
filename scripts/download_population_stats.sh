4b2a85c8c5d1bf62e45d770b505140f87d1219be
brendancol <brendan@makepath.com>
2021-09-08 16:10:09 -0500

    added summary header link download

--- scripts/download_population_stats.sh
+++ scripts/download_population_stats.sh
@@ -2,3 +2,7 @@ lftp -c 'mirror --parallel=100 https://www2.census.gov/programs-surveys/decennia
 mkdir -p population_stats
 find 01-Redistricting_File--PL_94-171 -name '*.pl.zip' -exec mv {} ./population_stats \;
 find ./population_stats -name '*.pl.zip' -execdir unzip {} \;
+
+cd popuplation_stats
+wget https://www2.census.gov/programs-surveys/decennial/rdo/about/2020-census-program/Phase3/SupportMaterials/2020_PLSummaryFile_FieldNames.xlsx
+cd -
