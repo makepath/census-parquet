a334b9edb359ae70c12d2ab086f6f4f0f17c6c88
Natalie O'Dell <67595848+nodell111@users.noreply.github.com>
2021-09-14 09:29:35 -0400

    Fixed spelling issue

--- scripts/download_population_stats.sh
+++ scripts/download_population_stats.sh
@@ -3,6 +3,6 @@ mkdir -p population_stats
 find 01-Redistricting_File--PL_94-171 -name '*.pl.zip' -exec mv {} ./population_stats \;
 find ./population_stats -name '*.pl.zip' -execdir unzip {} \;
 
-cd popuplation_stats
+cd population_stats
 wget https://www2.census.gov/programs-surveys/decennial/rdo/about/2020-census-program/Phase3/SupportMaterials/2020_PLSummaryFile_FieldNames.xlsx
 cd -
