ab9c3d5d8b3d851552d4b548c492d0fefacf3ef7
Natalie O'Dell <67595848+nodell111@users.noreply.github.com>
2021-09-14 09:28:54 -0400

    Update import statements

--- scripts/process_blocks.py
+++ scripts/process_blocks.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep  8 10:35:55 2021
-
-@author: natalieodell
-"""
 
 import dask.bag as bag
-import glob
-import pandas as pd
-import geopandas as gpd
 import dask_geopandas
-from os import path
+import geopandas as gpd
+import glob
 import numpy as np
-
 import os
+from os import path
+import pandas as pd
 import pyarrow.parquet as pq
+
 
 statelookup = {'01': 'AL', '02': 'AK', '04': 'AZ', '05': 'AR', '06': 'CA', '08': 'CO', '09': 'CT', '10': 'DE', '11': 'DC', '12': 'FL', '13': 'GA', '15': 'HI', '16': 'ID', '17': 'IL', '18': 'IN', '19': 'IA', '20': 'KS', '21': 'KY', '22': 'LA', '23': 'ME', '24': 'MD', '25': 'MA', '26': 'MI', '27': 'MN', '28': 'MS', '29': 'MO', '30': 'MT', '31': 'NE', '32': 'NV', '33': 'NH', '34': 'NJ', '35': 'NM', '36': 'NY', '37': 'NC', '38': 'ND', '39': 'OH', '40': 'OK', '41': 'OR', '42': 'PA', '44': 'RI', '45': 'SC', '46': 'SD', '47': 'TN', '48': 'TX', '49': 'UT', '50': 'VT', '51': 'VA', '53': 'WA', '54': 'WV', '55': 'WI', '56': 'WY', '72': 'PR'}
 
