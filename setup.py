a80aa247a1da13591763a15021eeb509fb959b22
brendancol <brendan@makepath.com>
2021-09-15 10:19:56 -0400

    quick fix

--- setup.py
+++ setup.py
@@ -4,9 +4,4 @@ from setuptools import setup
 setup(
     name='census-parquet',
     version='0.0.1',
-    entry_points={
-        'console_scripts': [
-            'mysupercoolscript=mysupercoolscript:mysupercoolscript'
-        ]
-    }
 )
