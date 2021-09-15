38b7005513353774909fc05dcd9b141413f175ea
brendancol <brendan@makepath.com>
2021-09-15 10:27:54 -0400

    updated setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
-#setup.py
-
 from setuptools import setup
+
 setup(
     name='census-parquet',
     version='0.0.1',
+    packages=['census_parquet'],
+    license='MIT',
+    description='Tools for generating Parquet files from Census 2020',
+    author='makepath',
+    url='https://github.com/makepath/census-parquet',
+    install_requires=[
+        'dask',
+        'openpyxl',
+        'pyarrow',
+    ],
 )
