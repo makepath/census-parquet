c09733bbeb0a7e5dfe386e74152ec7d81120fd91
Ian Thomas <ianthomas23@gmail.com>
2021-09-16 17:55:04 +0100

    Bump version number to 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='census-parquet',
-    version='0.0.5',
+    version='0.0.6',
     packages=['census_parquet'],
     license='MIT',
     description='Tools for generating Parquet files from US Census 2020',
