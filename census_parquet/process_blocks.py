7e467daf978fbd056f0169b62f814c202f88d7c3
Tom Augspurger <taugspurger@microsoft.com>
2021-09-20 13:30:11 -0500

    Updated process_blocks

--- census_parquet/process_blocks.py
+++ census_parquet/process_blocks.py
@@ -1,11 +1,25 @@
-import dask.bag as bag
+"""
+Processing Census Blocks.
+
+We create two logical tables:
+
+1. Geometries only
+2. Populations only
+
+This is driven by the Census Bureau not providing population statistics
+for territories (yet?).
+"""
+from pathlib import Path
+import warnings
+
+import dask
+import dask.dataframe as dd
 import dask_geopandas
-import geopandas as gpd
-import glob
-import numpy as np
-import os
+from dask.diagnostics import ProgressBar
+import geopandas
 import pandas as pd
-import pyarrow.parquet as pq
+
+warnings.filterwarnings("ignore", message=".*initial implementation of Parquet.*")
 
 
 statelookup = {
@@ -65,25 +79,13 @@ statelookup = {
 SUMMARY_TABLE = "./population_stats/2020_PLSummaryFile_FieldNames.xlsx"
 
 
-def add_population_stats(filename, gdf):
+def process_pop(file):
+    FIPS = file.stem.split("_")[2]
+    ABBR = statelookup[FIPS]
 
-    FIPS = os.path.split(filename)[-1].split("_")[2]
-    ABBR = statelookup.get(FIPS)
-
-    if not ABBR:
-        print(f"unable to parse FIPS:{FIPS}")
-        gdf["P0010001"] = np.empty(len(gdf), dtype="f8")
-        gdf["STUSAB"] = ABBR
-        return gdf
-
-    state_1 = f"./population_stats/{ ABBR.lower() }000012020.pl"
-    state_geo = f"./population_stats/{ ABBR.lower() }geo2020.pl"
-
-    if not os.path.exists(state_1) or not os.path.exists(state_geo):
-        print(f" unable to find {state_1}")
-        gdf["P0010001"] = np.zeros(len(gdf), dtype="f8") * np.nan
-        gdf["STUSAB"] = ABBR
-        return gdf
+    root = Path("population_stats")
+    state_1 = root / (ABBR.lower() + "000012020.pl")
+    state_geo = root / (ABBR.lower() + "geo2020.pl")
 
     seg_1_header_df = pd.read_excel(
         SUMMARY_TABLE, sheet_name="2020 P.L. Segment 1 Fields"
@@ -99,7 +101,7 @@ def add_population_stats(filename, gdf):
         delimiter="|",
         names=seg_1_header_df.columns.to_list(),
         low_memory=False,
-    )
+    ).drop(columns=["STUSAB"])
 
     geo_df = pd.read_csv(
         state_geo,
@@ -108,83 +110,110 @@ def add_population_stats(filename, gdf):
         names=geo_header_df.columns.to_list(),
         low_memory=False,
     )
+    geo_df = geo_df[geo_df["SUMLEV"] == 750]
 
-    pop_df = pd.merge(
-        left=geo_df[["LOGRECNO", "GEOID", "STUSAB", "BLOCK", "SUMLEV"]],
-        right=seg_1_df[["LOGRECNO", "P0010001"]],
+    block_df = pd.merge(
+        left=geo_df[["LOGRECNO", "GEOID", "STUSAB"]],
+        right=seg_1_df,
         how="left",
         on="LOGRECNO",
-    )
-
-    block_df = pop_df[pop_df.SUMLEV == 750]
-
+    ).drop(columns=["LOGRECNO", "CHARITER", "STUSAB", "FILEID", "CIFSN"])
     block_df["GEOID"] = block_df["GEOID"].str.replace("7500000US", "")
+    block_df = block_df.set_index("GEOID").sort_index()
 
-    updated_gdf = pd.merge(
-        left=gdf, right=block_df, how="left", left_on="GEOID20", right_on="GEOID"
+    assert block_df.index.is_unique
+    block_df = dd.from_pandas(block_df, npartitions=1)
+
+    output = Path(f"tmp/pop/{FIPS}.parquet")
+    output.parent.mkdir(parents=True, exist_ok=True)
+    block_df.to_parquet(output)
+
+    return output
+
+
+def process_geo(file):
+    dtypes = {
+        "STATEFP": "category",
+        "COUNTYFP": "category",
+        "TRACTCE": "int",
+        "BLOCKCE": "int",
+    }
+
+    gdf = (
+        geopandas.read_file(file, driver="SHP")
+        .drop(columns=["MTFCC20", "UR20", "UACE20", "UATYPE20", "FUNCSTAT20", "NAME20"])
+        .rename(columns=lambda x: x.rstrip("20"))
+        .astype(dtypes)
+        .set_index("GEOID")
     )
+    gdf["INTPTLON"] = pd.to_numeric(gdf["INTPTLON"])
+    gdf["INTPTLAT"] = pd.to_numeric(gdf["INTPTLAT"])
+    gdf = dask_geopandas.from_geopandas(gdf, npartitions=1)
 
-    updated_gdf["STUSAB"] = updated_gdf["STUSAB"].astype("category")
-    updated_gdf["P0010001"] = updated_gdf["P0010001"].astype(float)
+    output = Path(f"tmp/geo/{file.stem.split('_')[2]}.parquet")
+    output.parent.mkdir(parents=True, exist_ok=True)
+    gdf.to_parquet(output)
 
-    del updated_gdf["LOGRECNO"]
-    del updated_gdf["BLOCK"]
-    del updated_gdf["SUMLEV"]
-    del updated_gdf["GEOID"]
-
-    return updated_gdf
-
-
-def load(filename):
-    print(f"Started {filename}")
-    census_geom = gpd.read_file(filename, driver="SHP")
-    gdf = add_population_stats(filename, census_geom)
-
-    # dtype conversions
-    gdf["INTPTLON20"] = gdf["INTPTLON20"].astype(float)
-    gdf["INTPTLAT20"] = gdf["INTPTLAT20"].str.replace("+", "").astype(float)
-    gdf["STATEFP20"] = gdf["STATEFP20"].astype("category")
-    gdf["COUNTYFP20"] = gdf["COUNTYFP20"].astype("category")
-    gdf["TRACTCE20"] = gdf["TRACTCE20"].astype("int")
-    gdf["BLOCKCE20"] = gdf["BLOCKCE20"].astype("int")
-
-    # drop fields
-    del gdf["MTFCC20"]
-    del gdf["UR20"]
-    del gdf["UACE20"]
-    del gdf["UATYPE20"]
-    del gdf["FUNCSTAT20"]
-
-    # write to parquet
-    outputname = os.path.join("./outputs", os.path.split(filename)[-1] + ".parquet")
-    ddf = dask_geopandas.from_geopandas(gdf, npartitions=1)
-    ddf.to_parquet(outputname)
-    print(f"Finished {outputname}")
-    return filename
+    return output
 
 
-def combine_parquet_files(input_folder, target_path):
-    try:
-        files = []
+def process(file):
+    geo = process_geo(file)
+    FIPS = file.stem.split("_")[2]
 
-        for file_name in os.listdir(input_folder):
-            files.append(pq.read_table(os.path.join(input_folder, file_name)))
+    if FIPS in statelookup:
+        pop = process_pop(file)
+        result = pd.merge(geo, pop)
+        assert len(result) == len(geo)
+    else:
+        pop = None
 
-        for f in files:
-            pq.write_to_dataset(
-                f, root_path=target_path, partition_cols=["STATEFP20", "COUNTYFP20"]
-            )
-
-    except Exception as e:
-        print(e)
-        raise
+    return file, geo, pop
 
 
 def main():
-    files = glob.glob("./TABBLOCK20/*.zip")
-    bg = bag.from_sequence(files).map(load)
-    bg.compute()
-    combine_parquet_files("./outputs", "tl_2020_FULL_tabblock20.parquet")
+    files = list(Path("TABBLOCK20").glob("*.zip"))
+    geos = [dask.delayed(process_geo)(file) for file in files]
+    pops = [
+        dask.delayed(process_pop)(file)
+        for file in files
+        if file.stem.split("_")[2] in statelookup
+    ]
+    assert len(geos)
+    assert len(pops)
+
+    print("processing geo files")
+    with ProgressBar():
+        geo_files = dask.compute(*geos)
+
+    print("processing population files")
+    with ProgressBar():
+        pop_files = dask.compute(*pops)
+
+    pop = dd.concat([dd.read_parquet(f) for f in sorted(pop_files)])
+    geo = dd.concat([dask_geopandas.read_parquet(f) for f in sorted(geo_files)])
+    assert pop.known_divisions
+    assert geo.known_divisions
+
+    Path("outputs").mkdir(exist_ok=True)
+    print("finalizing population files")
+    with ProgressBar():
+        pop.to_parquet("outputs/census_blocks_population.parquet")
+
+    print("Computing spatial partitions")
+    with ProgressBar():
+        geo.calculate_spatial_partitions()
+
+    print("finalizing geo files")
+    with ProgressBar():
+        geo.to_parquet("outputs/census_blocks_geo.parquet")
+
+    print("validating")
+    a = dd.read_parquet("outputs/census_blocks_population.parquet")
+    assert a.known_divisions
+
+    b = dask_geopandas.read_parquet("outputs/census_blocks_geo.parquet")
+    assert b.known_divisions
 
 
 if __name__ == "__main__":
