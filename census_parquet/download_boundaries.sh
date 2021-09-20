569234ffb4b884d22e7948a26b74381df4e8c70d
Natalie O'Dell <nodell111@gwu.edu>
2021-09-20 08:30:02 -0400

    Added wget for nation outline

--- census_parquet/download_boundaries.sh
+++ census_parquet/download_boundaries.sh
@@ -4,3 +4,4 @@ cd census_boundaries
 wget https://www2.census.gov/geo/tiger/GENZ2020/shp/cb_2020_us_all_500k.zip
 unzip cb_2020_us_all_500k.zip
 rm cb_2020_us_all_500k.zip
+wget https://www2.census.gov/geo/tiger/GENZ2020/shp/cb_2020_us_nation_5m.zip
