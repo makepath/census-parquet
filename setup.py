bbb48f521d365a4a577f4795fc6b72d2a4e3a321
Ian Thomas <ianthomas23@gmail.com>
2022-01-04 17:42:52 +0000

    Prepare for v0.0.8 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='census-parquet',
-    version='0.0.7',
+    version='0.0.8',
     packages=['census_parquet'],
     license='MIT',
     description='Tools for generating Parquet files from US Census 2020',
