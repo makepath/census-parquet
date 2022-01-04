c93322f8478c141d8821d1a2326df310fcf62874
Ian Thomas <ianthomas23@gmail.com>
2022-01-04 17:53:20 +0000

    Ensure license file in sdist

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='census-parquet',
     version='0.0.8',
     packages=['census_parquet'],
-    license='MIT',
     description='Tools for generating Parquet files from US Census 2020',
     author='makepath',
     url='https://github.com/makepath/census-parquet',
